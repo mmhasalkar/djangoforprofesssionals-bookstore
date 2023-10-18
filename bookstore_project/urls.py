f5d9bdad3b52156fcb1fb9313625c669324cb9fd
Mukunda Mhasalkar <mmhasalkar@gmail.com>
2023-10-18 23:27:39 +0530

    Updating the auth methods (login, logout & signup)

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -17,6 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django Adming
     path('admin/', admin.site.urls),
+
+
+    # User Management
+    path('accounts/', include('django.contrib.auth.urls')),
+
+
+    # Local apps
+    path('accounts/', include('users.urls')),
     path('', include('pages.urls')),
 ]
