55f9d81e2033bab6bd64236dcfe20a0955ef7fa8
Mukunda Mhasalkar <mmhasalkar@gmail.com>
2023-10-22 10:22:14 +0530

    Revert "Adding smtp email services"
    This reverts commit 4f42a18b37f15b85c1e088a9afe028c1aace9239.

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -167,9 +167,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4' # new
 
 
 # Mail configurations
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'admin@megabookstore.com'
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = os.environ.get('EMAIL_PORT')
