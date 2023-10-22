4f42a18b37f15b85c1e088a9afe028c1aace9239
Mukunda Mhasalkar <mmhasalkar@gmail.com>
2023-10-22 10:17:37 +0530

    Adding smtp email services

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -167,5 +167,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4' # new
 
 
 # Mail configurations
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = 'admin@megabookstore.com'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
